6ad3f5bbade4cefb7a2d81b44ffed7f6acda5f13
Juan Campa <juancampa@gmail.com>
2023-05-14 19:58:54 -0400

    Default to US country code. Clarify

--- index.ts
+++ index.ts
@@ -2,8 +2,13 @@ import { root, nodes, state } from "membrane";
 const { sys_user } = nodes;
 
 export const Root = {
-  configure: async ({ args }) =>
-    await sys_user.configureSms({ ...args }).$invoke(),
+  configure: async ({ args }) => {
+    if (!args.number) {
+      throw new Error("Missing phone number");
+    }
+    const number = args.number.length === 10 ? `1${args.number}` : args.number;
+    return await sys_user.configureSms({ number }).$invoke();
+  },
 
   handleSms: async ({ args: { message } }) => {
     await root.received.$emit({ message });
@@ -13,4 +18,3 @@ export const Root = {
     await sys_user.sms.tell({ ...args }).$invoke();
   },
 };
-
